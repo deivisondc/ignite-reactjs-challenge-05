67377f4deeb984bf0f203b6de6cd6250d4375d3b
Deivison Cardoso <deivisondc@gmail.com>
2021-06-29 22:32:36 -0300

    fetch and show posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <div className={styles.container}>
+      <header>
+        <img src="/images/logo.svg" alt="Logo" />
+      </header>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main>
+        {postsPagination.results.map(post => (
+          <div className={styles.post}>
+            <strong>{post.data.title}</strong>
+            <p>{post.data.subtitle}</p>
 
-//   // TODO
-// };
+            <footer>
+              <span>{post.first_publication_date}</span>
+              <span>{post.data.author}</span>
+            </footer>
+          </div>
+        ))}
+
+        <a href="/">Carregar mais posts</a>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.Predicates.at('document.type', 'posts'),
+    { pageSize: 5 }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.id,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'd MMM yyyy'
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      },
+    },
+  };
+};
