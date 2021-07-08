c966294b43426282062cecf70adf6496c7c6f9e0
Deivison Cardoso <deivisondc@gmail.com>
2021-07-07 22:58:30 -0300

    fix to be compliance with test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <header>
-        <img src="/images/logo.svg" alt="Logo" />
+        <Link href="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </Link>
       </header>
     </div>
   );
